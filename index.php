271e98629f9df6aafd8279925d13e483c4752b97
Alfred Mwangi <mwangialfredkiguru@gmail.com>
2019-06-08 20:00:07 +0300

    initial Commit

---
+++ index.php
@@ -0,0 +1,195 @@
+﻿<?php include "partials/header.php" ?>
+<body>
+	<!-- main banner -->
+	<div class="main-top" id="home">
+		
+		<?php include "partials/nav.php" ?>
+		<!-- banner -->
+		<div class="banner_w3lspvt position-relative">
+			<div class="container">
+				<div class="d-md-flex">
+					<div class="w3ls_banner_txt">
+						<h3 class="w3ls_pvt-title">SimplaTech <br><span>Consulting</span></h3>
+						<p class="text-sty-banner">
+								Simple Solutions for Complex Connections. We specialize in offering services in ERP, Web and Mobile. 
+								All these services we offered using the lastest technology to give you unparralled competitive advantage.</p>
+						<a href="contact" class="btn button-style mt-md-5 mt-4">Contact Us</a>
+					</div>
+					<div class="banner-img">
+						<img src="images/banner.png" alt="" class="img-fluid">
+					</div>
+				</div>
+			</div>
+			<!-- animations effects 
+			<div class="icon-effects-w3l">
+				<img src="images/shape1.png" alt="" class="img-fluid shape-wthree shape-w3-one">
+				<img src="images/shape2.png" alt="" class="img-fluid shape-wthree shape-w3-two">
+				<img src="images/shape3.png" alt="" class="img-fluid shape-wthree shape-w3-three">
+				<img src="images/shape5.png" alt="" class="img-fluid shape-wthree shape-w3-four">
+				<img src="images/shape4.png" alt="" class="img-fluid shape-wthree shape-w3-five">
+				<img src="images/shape6.png" alt="" class="img-fluid shape-wthree shape-w3-six">
+			</div>
+			 //animations effects -->
+		</div>
+		<!-- //banner -->
+	</div>
+	<!-- //main banner -->
+
+	<!-- what we do section -->
+	<div class="what bg-li py-5" id="what">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="row about-bottom-w3l text-center mt-4">
+				<div class="col-lg-4 about-grid">
+					<div class="about-grid-main">
+						<img src="images/img1.png" alt="" class="img-fluid">
+						<h4 class="my-4">ALL ABOUT THE QUALITY</h4>
+						<p> When it comes to developing custom, enterprise solutions, Quality of the application should never be an after-thought.</p>
+						<!--<a href="about" class="button-w3ls btn mt-sm-5 mt-4">Read More</a>-->
+					</div>
+				</div>
+				<div class="col-lg-4 about-grid my-lg-0 my-5">
+					<div class="about-grid-main">
+						<img src="images/img2.png" alt="" class="img-fluid">
+						<h4 class="my-4">WE SPEAK YOUR LANGUAGE</h4>
+						<p>We only use terminology your employees are familiar with and only add the functionality you actually require.</p>
+						<!--<a href="about" class="button-w3ls btn mt-sm-5 mt-4">Read More</a>-->
+					</div>
+				</div>
+				<div class="col-lg-4 about-grid">
+					<div class="about-grid-main">
+						<img src="images/img3.png" alt="" class="img-fluid">
+						<h4 class="my-4">TIMELY DELIVERLY</h4>
+						<p>We value customers time, thats why we do deliver and satisfy the customers. We always endeavour to make the software work for you.</p>
+						<!--<a href="about" class="button-w3ls btn mt-sm-5 mt-4">Read More</a>-->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //what we do section -->
+
+	<!-- middle -->
+	<section class="midd-w3 py-5" id="faq">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="row">
+				<div class="col-lg-6 about-right-faq">
+					<h6>Business Consultancy</h6>
+					<h3 class="text-da">Years of Industry Experience</h3>
+					<p class="mt-4">Equipped with a wealth of experience of nearly 5 years in software development in Kenya.
+						 We have helped businesses solve problems with custom software.</p>
+					<ul class="w3l-right-book mt-4">
+						<li>ERP Solutions</li>
+						<li>Software Consultancy</li>
+						<li>API Development</li>
+						<li>Systems Intergrations</li>
+						<li>Technical Support</li>
+					</ul>
+					<a href="about" class="btn button-style button-style-2 mt-sm-5 mt-4">Read More</a>
+				</div>
+				<div class="col-lg-6 left-wthree-img text-right">
+					<img src="images/b1.png" alt="" class="img-fluid mt-5" />
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //middle -->
+
+	<!-- services -->
+	<section class="banner-bottom-w3layouts bg-li py-5" id="services">
+		<div class="container py-xl-5 py-lg-3">
+			<h3 class="tittle text-center font-weight-bold">Our Services</h3>
+			<p class="sub-tittle text-center mt-3 mb-sm-5 mb-4">Software Solutions
+					to match your needs</p>
+			<div class="row pt-lg-4">
+				<div class="col-lg-4 about-in text-center">
+					<div class="card">
+						<div class="card-body">
+							<div class="bg-clr-w3l">
+								<span class="fa fa-credit-card-alt"></span>
+							</div>
+							<h5 class="card-title mt-4 mb-3">ERP development</h5>
+							<p class="card-text">Tailor made financial software business solutions and clear implementation to match unique end-user requirments.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 about-in text-center my-lg-0 my-3">
+					<div class="card active">
+						<div class="card-body">
+							<div class="bg-clr-w3l">
+								<span class="fa fa-mobile"></span>
+							</div>
+							<h5 class="card-title mt-4 mb-3">Mobile Development</h5>
+							<p class="card-text">We create mobile applications for iOS, Android and Windows Phone, natively and cross platform.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 about-in text-center">
+					<div class="card">
+						<div class="card-body">
+							<div class="bg-clr-w3l">
+								<span class="fa fa-chrome"></span>
+							</div>
+							<h5 class="card-title mt-4 mb-3">Web Development</h5>
+							<p class="card-text">Our development team ensures your website is lightweight, easily expandable, search engine friendly.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //services -->
+
+	<!-- stats 
+	<section class="bottom-count py-5" id="stats">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="row">
+				<div class="col-lg-5 left-img-w3ls">
+					<img src="images/b2.png" alt="" class="img-fluid" />
+				</div>
+				<div class="col-lg-7 right-img-w3ls pl-lg-4 mt-lg-2 mt-4">
+					<div class="bott-w3ls mr-xl-5">
+						<h3 class="title-w3 text-bl mb-3 font-weight-bold">Some of Company Real Facts</h3>
+						<p class="title-sub-2 mb-3">Excepteur sint occaecat cupidatat <br>non proident.</p>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+							laudantium. </p>
+					</div>
+					<div class="row mt-5">
+						<div class="col-sm-4 count-w3ls">
+							<h4>1000+</h4>
+							<p>Project completed</p>
+						</div>
+						<div class="col-sm-4 count-w3ls mt-sm-0 mt-3">
+							<h4>480+</h4>
+							<p>Consultant</p>
+						</div>
+						<div class="col-sm-4 count-w3ls mt-sm-0 mt-3">
+							<h4>600+</h4>
+							<p>Award Winning</p>
+						</div>
+					</div>
+					<div class="row mt-sm-5 mt-4">
+						<div class="col-sm-4 count-w3ls">
+							<h4>480+</h4>
+							<p>Consultant</p>
+						</div>
+						<div class="col-sm-4 count-w3ls mt-sm-0 mt-3">
+							<h4>600+</h4>
+							<p>Award Winning</p>
+						</div>
+						<div class="col-sm-4 count-w3ls mt-sm-0 mt-3">
+							<h4>1000+</h4>
+							<p>Project completed</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	 //stats -->
+
+	
+
+
+
+<?php include "partials/footer.php" ?>
+</html>
---
+++ partials/nav.php
@@ -0,0 +1,26 @@
+<!-- header -->
+<header>
+    <div class="container-fluid">
+        <div class="header d-lg-flex justify-content-between align-items-center py-3 px-sm-3">
+            <!-- logo -->
+            <div id="logo">
+                <h1><a href="index">
+                    <!--<span class="fa fa-cloud-download mr-2"></span>-->
+                    SimplaTech</a></h1>
+            </div>
+            <!-- //logo -->
+            <!-- nav -->
+            <div class="nav_w3ls">
+                <nav>
+                    <label for="drop" class="toggle">Menu</label>
+                    <input type="checkbox" id="drop" />
+                    <ul class="menu">
+                        <li><a href="index.php" class="active">Home</a></li>                  
+                        <li><a href="contact.php">Contact Us</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- //header -->
